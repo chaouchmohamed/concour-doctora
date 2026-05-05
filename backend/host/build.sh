bbe17f5c8bd874588d372e3d448e3158ef5c095a
aboubakr <aboubakr@gmail.com>
2026-05-05 11:28:42 +0100

    fix build.sh file

--- backend/host/build.sh
+++ backend/host/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install -r requirements.txt/prod.txt
+pip install -r requirements/prod.txt
 python manage.py collectstatic --no-input --settings=config.settings.prod
 python manage.py migrate --settings=config.settings.prod
